8c42331d6396d31c98b8989e0bb692da8254dbf3
Ersel ÇAKMAK <101339022+ErselCakmak@users.noreply.github.com>
2023-04-25 10:46:33 +0300

    ready

---
+++ android/app/src/main/kotlin/com/contract/qr_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.contract.qr_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_generator/crypt.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QR Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _qrData = Crypt().set(DateFormat('dd-MM-yyyy HH:mm:ss').format(DateTime.now()), "encrypt");
+  int _start = 30;
+  bool _visible = true;
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(
+      oneSec,
+      (Timer timer) => setState(
+        () {
+          if (_start <= 1) {
+            _visible = false;
+            _qrData = Crypt().set(DateFormat('dd-MM-yyyy HH:mm:ss').format(DateTime.now()), "encrypt");
+            _start = 30;
+          } else {
+            _visible = true;
+            _start = _start - 1;
+          }
+        },
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Spacer(),
+            const Text(
+              'Giriş-Çıkış işlemleri için QR kodunu okutunuz.',
+              style: TextStyle(fontSize: 24),
+            ),
+            const Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.0),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: _visible
+                    ? QrImage(
+                        data: _qrData,
+                        version: QrVersions.auto,
+                        size: width * .25,
+                      )
+                    : SizedBox(
+                        width: width * .25,
+                        height: width * .25,
+                        child: const Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.amber,
+                          ),
+                        ),
+                      ),
+              ),
+            ),
+            const Spacer(),
+            Text(
+              ' Kodun yenilenmesine $_start saniye kaldı.',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
